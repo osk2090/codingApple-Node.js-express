b022d2ea2d8656e8f9f5d1c4c8089bd1d48947ce
osk2090 <osk2090@naver.com>
2022-06-26 03:33:59 +0900

    기본 구조 파악

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ app.get('/pet', function (요청, 응답) {
 app.get('/beauty', function (요청, 응답) {
     응답.send('뷰티용품을 쇼핑할 수 있는 페이지입니다.');
 });
+
+app.get('/', function (요청, 응답) {
+    응답.sendFile(__dirname + '/index.html');
+});
